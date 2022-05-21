ee4f9a093f25381658fe42d562bcf814f5fff6d8
happy8131 <lion8131@naver.com>
2022-05-21 23:56:33 +0900

    [ADD] ScrollView, Dimensions, pagingEnabled horizontal

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
-import React from "react";
+import * as Location from "expo-location";
+import React, { useEffect, useState } from "react";
 import { View, Text, Dimensions, StyleSheet, ScrollView } from "react-native";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+console.log(SCREEN_WIDTH);
 
 export default function App() {
   return (
@@ -7,12 +11,29 @@ export default function App() {
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}> Sunny</Text>
         </View>
-      </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}> Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}> Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}> Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -32,10 +53,10 @@ const styles = StyleSheet.create({
     fontWeight: "500",
   },
   weather: {
-    flex: 2,
+    backgroundColor: "blue",
   },
   day: {
-    flex: 1,
+    width: SCREEN_WIDTH,
     alignItems: "center",
   },
   temp: {
