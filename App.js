1fce21e52bff1d78e24c23a1c8f68eae185e680e
happy8131 <lion8131@naver.com>
2022-05-21 23:26:42 +0900

    [ADD] style

--- App.js
+++ App.js
@@ -1,15 +1,52 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text, Dimensions, StyleSheet, ScrollView } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, flexDirection: "row" }}>
-      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
-      <View style={{ flex: 1, backgroundColor: "teal" }}></View>
-      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}> Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 38,
+    fontWeight: "500",
+  },
+  weather: {
+    flex: 2,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+  },
+});
 
 //view는 컨테이너고 항상 임포트를 해야한다 , 그리고 text는 모든 text component에 들어d가야한다.
 /*
